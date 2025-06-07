0d44aedd99574c32c65a1d2627087211bb41e456
Janhavi P.  Somnathe <janhavisomnathe@gmail.com>
2025-06-07 23:31:07 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,36 +1,38 @@
-package org.example;
-
 public class Student {
     private int studentId;
     private String studentName;
 
-}
-/*public Student(int studentId, String studentName){
-    //this.studentId = studentId;
- //    this.studentName = studentName;
+    /*public Student(int studentId, String studentName){
+            //this.studentId = studentId;
+         //    this.studentName = studentName;
 
-//}*/
-public int getStudentId(){
-    return studentId;
-}
-public void setStudentId(int studentId){
-    this.studentId = studentId;
-
-}
-
-public String getStudentName(){
-    return studentName;
-}
-public void setStudentName(String studentName){
-    this.studentName = studentName;
-}
-class Tester {
-    public static void main(String arg[]){
-        Student student = new Student();
-        student.setStudentId("c1");
-        student.setStudentName("Janhavi");
+        //}*/
+    public int getStudentId(){
+        return studentId;
     }
-    system.out.println("Displaying student details:");
-    system.out.println("Student Id :"+ student.getStudentId());
 
+    public void setStudentId(int studentId){
+        this.studentId = studentId;
+
+    }
+
+    public String getStudentName(){
+        return studentName;
+    }
+
+    public void setStudentName(String studentName){
+        this.studentName = studentName;
+    }
+}
+
+class Tester {
+    public static void main(String arg[]) {
+        Student student = new Student();
+        student.setStudentId(1);
+        student.setStudentName("Janhavi");
+
+        System.out.println("Displaying student details:");
+        System.out.println("Student Id :" + student.getStudentId());
+        System.out.println("Student Name: " + student.getStudentName());
+    }
 }
